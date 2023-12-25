e1c3b319cf157ba7bf62e76cc33c66e2575ed2d6
rahulrangers <rahulreddhypurmani123@gmail.com>
2023-12-25 20:47:45 +0530

    added-contact

---
+++ AnyKart/src/Components/Contact.tsx
@@ -0,0 +1,31 @@
+import { Card } from "@mui/material"
+import React from "react"
+const Contact = ()=>{
+return(
+    <>
+    <div className=" my-4 flex justify-center  text-4xl  font-bold">
+        Contact Us
+    </div>
+    <div className="my-3 p-2  md:grid md:grid-cols-3 " >
+  <div  className=" py-20  h-96 text-2xl m-1  bg-slate-600 rounded-3xl font-extrabold">
+ <div className="flex justify-center">Address</div>
+ <div className="flex justify-center ">IIT jodhpur,Jodhpur</div>
+<div className="flex justify-center">Rajasthan,India</div> 
+ 
+  </div>
+    <div className=" py-20 text-2xl font-extrabold m-1 bg-slate-600 rounded-3xl">
+        <div className="flex justify-center" >Phone</div>
+        <div className="flex justify-center">You can contact us through Mobile</div>
+        <div className="flex justify-center">Call now to : 87860404xxx</div>
+        </div>  
+        <div   className=" py-16 text-2xl font-extrabold m-1 bg-slate-600 rounded-3xl">
+            <div className="flex justify-center">Email</div>
+            <div className="flex justify-center">You can ask your doubts and </div>
+            <div className="flex justify-center"> questions by sending us emails </div>
+            <div className="flex justify-center">Email: contact@gmail.com</div>
+        </div>
+    </div>
+    </>
+)
+}
+export default Contact
